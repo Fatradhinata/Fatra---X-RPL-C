d90bbc2f2a9921490948c74277ebb7b2fd25bc86
Fatradhinata <79489643+Fatradhinata@users.noreply.github.com>
2022-09-28 10:26:53 +0700

    Membuat file info.php

---
+++ info.php
@@ -0,0 +1,30 @@
+
+<!doctype html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<p>&lt;!doctype html&gt;<br />
+&lt;title&gt;Website Check Page&lt;/title&gt;<br />
+&lt;style&gt;<br />
+&nbsp; body { text-align: center; padding: 150px; }<br />
+&nbsp; h1 { font-size: 50px; }<br />
+&nbsp; body { font: 20px Helvetica, sans-serif; color: #333; }<br />
+&nbsp; article { display: block; text-align: left; width: 650px; margin: 0 auto; }<br />
+&nbsp; a { color: #dc8100; text-decoration: none; }<br />
+&nbsp; a:hover { color: #333; text-decoration: none; }<br />
+&lt;/style&gt;</p>
+
+<p>&lt;article&gt;<br />
+&nbsp; &nbsp; &lt;h1&gt;We&amp;rsquo;re Jagoan Cloud!&lt;/h1&gt;<br />
+&nbsp; &nbsp; &lt;div&gt;<br />
+&nbsp; &nbsp; &nbsp; &nbsp; &lt;p&gt;Helloo SMKN 4 Malang, this is check page only!&lt;/p&gt;<br />
+&nbsp; &nbsp; &nbsp; &nbsp; &lt;p&gt;Ilham Azis - XIRPL2&lt;/p&gt;<br />
+&nbsp; &nbsp; &nbsp; &nbsp; &lt;p&gt;Surya Wardhana - XIRPL2&lt;/p&gt;<br />
+&nbsp; &nbsp; &nbsp; &nbsp; &lt;p&gt;&amp;mdash; Thanks&lt;/p&gt;<br />
+&nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<br />
+&nbsp; &nbsp; &lt;/div&gt;<br />
+&lt;/article&gt;</p>
+</body>
+</html>
